6c73e0b1630b7709433f41c55062edd1bfabe436
anapsantos1 <paulalimas@gmail.com>
2021-09-07 18:29:19 -0300

    Alteração na rota incluir aluno

--- src/controllers/agendaController.js
+++ src/controllers/agendaController.js
@@ -132,6 +132,7 @@ const IncluirAluno = async (req, res) => {
       aulaID,
       { $push: { alunos: req.body.id } },
       { new: true, useFindAndModify: false })
+      Agenda.populate("alunos")
 
       //populate("alunos")
 
