bf2971138e489986f080c5d783b8ac7fa481de42
anapsantos1 <paulalimas@gmail.com>
2021-09-07 18:22:58 -0300

    Alteração na rota incluir aluno

--- src/controllers/agendaController.js
+++ src/controllers/agendaController.js
@@ -4,7 +4,8 @@ const Aluno = require('../models/alunos')
 const Professor = require('../models/professores')
 //const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const SECRET = process.env.SECRET
+const SECRET_ALUNO = process.env.SECRET_ALUNO
+const SECRET_PROFESSOR = process.env.SECRET_PROFESSOR
 
 const getAll = async (req, res) => {
   const authHeader = req.get('Authorization');
@@ -14,7 +15,7 @@ const getAll = async (req, res) => {
     return res.status(403).send({message: "Kd a autorizationnn"})
   }
 
-  jwt.verify(token, SECRET_ALUNO, async (err) => {
+  jwt.verify(token, SECRET_ALUNO, SECRET_PROFESSOR, async (err) => {
     if (err){
       res.status(403).send({message: '  token não valido', err})
     }
