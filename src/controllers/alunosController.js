4afd43b7ab09b92dca639318291ba9c4a9d58185
anapsantos1 <paulalimas@gmail.com>
2021-09-09 20:41:31 -0300

    Corrigindo rota delete aluno

--- src/controllers/alunosController.js
+++ src/controllers/alunosController.js
@@ -148,12 +148,13 @@ const getAll = async (req, res) => {
       res.status(500).send({message: err.message})
   }else{
     if(aluno){
-      Agenda.findOne({"id": alunoID}, function (err, agenda){
+      Agenda.findOne({id: alunoID}, function (err, agenda){
         if(agenda){
           res.status(500).send({
             message:'Possui uma aula Agendada',
             status: "FAIL"
           })
+        }else{
       aluno.deleteOne({id: alunoID}, function(err){
         if (err){
           res.status(500).send({
@@ -167,7 +168,7 @@ const getAll = async (req, res) => {
         })
         }
       })
-        }
+    } 
       })
 
     }else {
