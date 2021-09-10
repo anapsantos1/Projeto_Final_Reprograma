3630b6b45c2c538ae692b34b34f943296899fc82
anapsantos1 <paulalimas@gmail.com>
2021-09-09 21:07:02 -0300

    Corrigindo rota delete aluno

--- src/controllers/alunosController.js
+++ src/controllers/alunosController.js
@@ -158,13 +158,13 @@ const removeOneAluno = async (req, res) => {
     if (aluno == null){
       return res.status(404).json({message: "Aluno  não encontrado"})
     }
-    const aulaAgendada = await Agenda.findOne({"id": aluno})
+    const aulaAgendada = await Agenda.findOne({turma:[{"id": aluno}]})
     if(aulaAgendada){
       res.status(404).json({message: 'Existe uma aula agendada, Por favor cancelar a aula antes de remover o professor'})
-    }else{
+    }
     aluno.remove()
     res.status(200).json({"mensagem":"Aluno removido com sucesso"})
-    }
+    
 
   }catch (err){
     res.status(500).json({message: err.message})
